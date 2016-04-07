ebacad811ebbed0379f6e671b6ba2369502fae49
Zach Schultz <zacharymschultz@gmail.com>
2016-04-07 18:12:46 -0400

    Uncommented AreaEffect from Tile now that we have abstract base class

--- src/com/wrathOfLoD/Models/Map/Tile.java
+++ src/com/wrathOfLoD/Models/Map/Tile.java
@@ -2,6 +2,7 @@ package com.wrathOfLoD.Models.Map;
 
 import com.wrathOfLoD.Models.Entity.Entity;
 import com.wrathOfLoD.Models.Items.Item;
+import com.wrathOfLoD.Models.Map.AreaEffect.AreaEffect;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +13,13 @@ import java.util.List;
 public class Tile {
     private List<Entity> entities;
     private List<Item> items;
-//    private List<AreaEffect> areaEffects;
+    private List<AreaEffect> areaEffects;
 //    private Flow flow;
 
     public Tile() {
         this.entities = new ArrayList<>();
         this.items = new ArrayList<>();
-//        this.areaEffects = new ArrayList<>();
+        this.areaEffects = new ArrayList<>();
     }
 
     public void add(Entity e) {
@@ -27,9 +28,9 @@ public class Tile {
     public void add(Item i) {
         items.add(i);
     }
-//    public void add(AreaEffect ae) {
-//        areaEffects.add(ae);
-//    }
+    public void add(AreaEffect ae) {
+        areaEffects.add(ae);
+    }
 
     public void removeEntity(Entity e) {
         entities.remove(e);
@@ -39,9 +40,9 @@ public class Tile {
         items.remove(i);
     }
 
-//    public void removeAE(AreaEffect ae) {
-//        areaEffects.remove(ae);
-//    }
+    public void removeAE(AreaEffect ae) {
+        areaEffects.remove(ae);
+    }
 
     public void interact(Entity e) {}
 }
