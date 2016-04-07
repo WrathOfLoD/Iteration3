89a58b0b225a7871eba26658fb7d3498733e87a7
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-04-07 14:08:46 -0400

    Add basic Items Folder and abstract Item Class

---
+++ src/com/wrathOfLoD/Models/Items/OneShotItem.java
@@ -0,0 +1,13 @@
+package com.wrathOfLoD.Models.Items;
+
+import com.wrathOfLoD.Utility.Position;
+
+/**
+ * Created by matthewdiaz on 4/7/16.
+ */
+public class OneShotItem extends Item{
+
+    public OneShotItem(Position position, String name){
+        super(position, name);
+    }
+}
