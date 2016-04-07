2996a46eec8cfbaebd72a8de0fa0db8709c15166
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-04-07 17:37:01 -0400

    Remove attribute name from Stats.java and moved it to Entity. Add necessary attributes to Entity

--- src/com/wrathOfLoD/Models/Entity/Entity.java
+++ src/com/wrathOfLoD/Models/Entity/Entity.java
@@ -1,22 +1,38 @@
 package com.wrathOfLoD.Models.Entity;
 
 import com.wrathOfLoD.Models.Entity.Character.Character;
+import com.wrathOfLoD.Models.Stats.Stats;
 import com.wrathOfLoD.Utility.Position;
 
 /**
  * Created by zach on 4/7/16.
  */
 public abstract class Entity {
+    private String name;
     private Position position;
+    private Stats stats;
 //     private Direction direction;
 
     public Entity() {
 
     }
 
+    public Entity(String name, Stats stats, Position position){
+        this.name = name;
+        this.stats = stats;
+        this.position = position;
+    }
+
+
 //    public void move(Direction d) {
 //
 //    }
+
+    public String getName(){ return this.name; }
+
+    public Stats getStats(){ return this.stats; }
+
+    public Position getPosition() { return this.position; }
 
     public void doInteration(Character character) {}
 
@@ -24,6 +40,6 @@ public abstract class Entity {
 
     public void levelUp() {}
 
-    public Position getPosition() { return this.position; }
+
 }
 
