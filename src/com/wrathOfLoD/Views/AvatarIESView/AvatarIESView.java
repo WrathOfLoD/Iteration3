577f9558c1db7e873d46da2ac809a8dc54130a61
Christiansen <echristiansen@ufl.edu>
2016-04-08 21:59:35 -0400

    updates

--- src/com/wrathOfLoD/Views/AvatarIESView/AvatarIESView.java
+++ src/com/wrathOfLoD/Views/AvatarIESView/AvatarIESView.java
@@ -8,42 +8,69 @@ import com.wrathOfLoD.Views.StaticView;
 import com.wrathOfLoD.Views.StatsView.StatsView;
 import com.wrathOfLoD.Views.View;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by echristiansen on 4/8/2016.
  */
 public class AvatarIESView extends StaticView {
 
-    private InventoryView iv;
-    private StatsView sv;
-    private EquipmentView ev;
+    private InventoryView inventoryView;
+    private StatsView statsView;
+    private EquipmentView equipmentView;
+    private StaticView containerPanel;
+    private JPanel pushPanel;
 
     private static final int AVATARIES_VIEW_WIDTH =(int) Config.instance().getAvatarIESViewWidth();
     private static final int AVATARIES_VIEW_HEIGHT = (int) Config.instance().getAvatarIESViewHeight();
 
     public InventoryView getInventoryView() {
-        return iv;
+        return inventoryView;
     }
     public void setInventoryView(InventoryView iv) {
-        this.iv = iv;
+        this.inventoryView = iv;
     }
     public StatsView getStatsView() {
-        return sv;
+        return statsView;
     }
     public void setStatsView(StatsView sv) {
-        this.sv = sv;
+        this.statsView = sv;
     }
     public EquipmentView getEquipmentView() {
-        return ev;
+        return equipmentView;
     }
     public void setEquipmentView(EquipmentView ev) {
-        this.ev = ev;
+        this.equipmentView = ev;
     }
 
     public AvatarIESView(InventoryView iv, StatsView sv, EquipmentView ev) {
         setInventoryView(iv);
         setStatsView(sv);
         setEquipmentView(ev);
-        setBackgroundImageFileName("resources/slothSpaceTrade.png");
+        //setBackgroundImageFileName("resources/slothSpaceTrade.png");
+        initDefaultUI();
+    }
+
+    public void initDefaultUI() {
+
+        pushPanel = new JPanel(); //this JPanel is used to center the CharacterView in the ViewManager. It "pushes" down the CharacterView in the ViewManager
+        pushPanel.setPreferredSize(new Dimension(Config.instance().getAreaViewWidth(), Config.instance().getAreaViewHeight() / 6));
+        pushPanel.setBackground(new Color(0f, 0f, 0f, 0f)); //pushPanel needs to be invisible
+        this.setBackground(new Color(0f, 0f, 0f, 0f));
+        this.setLayout(new FlowLayout());
+        this.containerPanel = new StaticView();
+        //this.containerPanel.setBackground(new Color(0f,0f,0f,0f));
+        this.containerPanel.setBackgroundImageFileName("resources/spaceSlothTrade.png");
+        this.containerPanel.setPreferredSize(new Dimension(AVATARIES_VIEW_WIDTH, AVATARIES_VIEW_HEIGHT));
+        this.containerPanel.setLayout(new GridLayout(1, 3));
+        this.containerPanel.add(this.inventoryView);
+        this.containerPanel.add(this.equipmentView);
+        this.containerPanel.add(this.statsView);
+        this.add(pushPanel);
+        this.add(containerPanel);
+        this.containerPanel.setBorder(BorderFactory.createStrokeBorder(new BasicStroke(15.0f)));
+
     }
 
 
