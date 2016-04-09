386b756dee00978cfe278092da105555cce8e65d
mburgunder <mitchellburgunder@yahoo.com>
2016-04-09 15:57:47 -0400

    vector operations in Position and Direction

--- src/com/wrathOfLoD/Utility/Position.java
+++ src/com/wrathOfLoD/Utility/Position.java
@@ -27,6 +27,17 @@ public class Position{
 		this.h = h;
 	}
 
+	public static Position copyOf(Position pos){
+		return new Position(pos.getQ(), pos.getR(), pos.getS(), pos.getH());
+	}
+
+	public void modifyPosition(Position target){
+		this.q = target.getQ();
+		this.r = target.getR();
+		this.s = target.getS();
+		this.h = target.getH();
+	}
+
 	public int getQ(){
 		return q;
 	}
@@ -69,6 +80,30 @@ public class Position{
 		return hash;
 	}
 
+	public static Position vectorAdd(Position a, Position b){
+		int abQ = a.getQ() + b.getQ();
+		int abR = a.getR() + b.getR();
+		int abS = a.getS() + b.getS();
+		int abH = a.getH() + b.getH();
+		return new Position(abQ, abR, abS, abH);
+	}
+
+	public static Position vectorSubtract(Position a, Position b){
+		int abQ = a.getQ() - b.getQ();
+		int abR = a.getR() - b.getR();
+		int abS = a.getS() - b.getS();
+		int abH = a.getH() - b.getH();
+		return new Position(abQ, abR, abS, abH);
+	}
+
+	public static Position scalarMultiply(Position a, int c){
+		int acQ = a.getQ() * c;
+		int acR = a.getR() * c;
+		int acS = a.getS() * c;
+		int acH = a.getH() * c;
+		return new Position(acQ, acR, acS, acH);
+	}
+
 	public int getDistance(Position pos){
 		int distanceHorz = Math.abs((this.getQ() - pos.getQ()) +
 				(this.getR() - pos.getR()) + (this.getS() - pos.getS()));
@@ -77,7 +112,7 @@ public class Position{
 	}
 
 	public Position getPosInDir(Direction dir){
-		return dir.getPosInDir(this);
+		return vectorAdd(this, dir.getPosVector());
 	}
 
 	public Position get2DProjection(){
