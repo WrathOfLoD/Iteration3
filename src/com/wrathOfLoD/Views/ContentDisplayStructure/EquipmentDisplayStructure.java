e38fbd129f417d642b57d1fda3f6a521f696773b
Christiansen <echristiansen@ufl.edu>
2016-04-09 21:31:56 -0400

    view updates

--- src/com/wrathOfLoD/Views/ContentDisplayStructure/EquipmentDisplayStructure.java
+++ src/com/wrathOfLoD/Views/ContentDisplayStructure/EquipmentDisplayStructure.java
@@ -1,5 +1,7 @@
 package com.wrathOfLoD.Views.ContentDisplayStructure;
 
+import com.sun.prism.image.Coords;
+
 import java.awt.*;
 
 /**
@@ -7,12 +9,22 @@ import java.awt.*;
  */
 public class EquipmentDisplayStructure {
 
-    private int structureWidth;
-    private int structureHeight;
+    private int structureWidth; //this is really the width of the view in which the structure is found
+    private int structureHeight; //this is reallly the height of the view in which the structure is found
     private int slotWidth;
     private int slotHeight;
     private int widthDivisor = 10; //used to determine how wide a slot should be relative to the overall width of the structure
     private int heightDivisor = 10;//used to determine how tall a slot shoudl be relative to the overall width of the structure
+    private int helmetX;
+    private int helmetY;
+    private int weaponX;
+    private int weaponY;
+    private int chestX;
+    private int chestY;
+    private int shieldX;
+    private int shieldY;
+    private int legsX;
+    private int legsY;
 
     public int getSlotWidth() {
         return slotWidth;
@@ -39,17 +51,92 @@ public class EquipmentDisplayStructure {
         this.structureHeight = structureHeight;
     }
 
+
+    public int getHelmetX() {
+        return helmetX;
+    }
+    public void setHelmetX(int helmetX) {
+        this.helmetX = helmetX;
+    }
+    public int getHelmetY() {
+        return helmetY;
+    }
+    public void setHelmetY(int helmetY) {
+        this.helmetY = helmetY;
+    }
+    public int getWeaponX() {
+        return weaponX;
+    }
+    public void setWeaponX(int weaponX) {
+        this.weaponX = weaponX;
+    }
+    public int getWeaponY() {
+        return weaponY;
+    }
+    public void setWeaponY(int weaponY) {
+        this.weaponY = weaponY;
+    }
+    public int getChestX() {
+        return chestX;
+    }
+    public void setChestX(int chestX) {
+        this.chestX = chestX;
+    }
+    public int getChestY() {
+        return chestY;
+    }
+    public void setChestY(int chestY) {
+        this.chestY = chestY;
+    }
+    public int getShieldX() {
+        return shieldX;
+    }
+    public void setShieldX(int shieldX) {
+        this.shieldX = shieldX;
+    }
+    public int getShieldY() {
+        return shieldY;
+    }
+    public void setShieldY(int shieldY) {
+        this.shieldY = shieldY;
+    }
+    public int getLegsX() {
+        return legsX;
+    }
+    public void setLegsX(int legsX) {
+        this.legsX = legsX;
+    }
+    public int getLegsY() {
+        return legsY;
+    }
+    public void setLegsY(int legsY) {
+        this.legsY = legsY;
+    }
+
+
     public EquipmentDisplayStructure(int viewWidth, int viewHeight) {
         setStructureWidth(viewWidth);
         setStructureHeight(viewHeight);
         setSlotWidth(getStructureWidth()/widthDivisor);
         setSlotHeight(getStructureHeight()/heightDivisor);
+        calculateCoords();
     }
 
-    public int calculateXCoordFromSlot(int slot){
-        int horLineLength = getStructureWidth()/4;
-        int x = slot*horLineLength-getSlotWidth()/2;
-        return x;
+
+    public void calculateCoords() {
+        int horlineLength = getStructureWidth()/4;
+        int vertLineLength = getStructureHeight()/4;
+        setHelmetX(2*horlineLength-getSlotWidth()/2);
+        setWeaponX(horlineLength-getSlotWidth()/2);
+        setChestX(2*horlineLength-getSlotWidth()/2);
+        setShieldX(3*horlineLength-getSlotWidth()/2);
+        setLegsX(2*horlineLength-getSlotWidth()/2);
+
+        setHelmetY(vertLineLength-getslotHeight()/2);
+        setWeaponY(2*vertLineLength-getslotHeight()/2);
+        setChestY(2*vertLineLength-getslotHeight()/2);
+        setShieldY(2*vertLineLength-getslotHeight()/2);
+        setLegsY(3*vertLineLength-getslotHeight()/2);
     }
 
 
--- src/com/wrathOfLoD/Views/ItemDisplayView/EquipmentView.java
+++ src/com/wrathOfLoD/Views/ItemDisplayView/EquipmentView.java
@@ -2,6 +2,8 @@ package com.wrathOfLoD.Views.ItemDisplayView;
 
 
 import com.wrathOfLoD.Views.ContentDisplayStructure.EquipmentDisplayStructure;
+import com.wrathOfLoD.Views.ViewObjects.EquippedItemViewObject;
+import com.wrathOfLoD.Views.ViewObjects.ItemViewObject;
 
 import javax.swing.border.BevelBorder;
 import javax.swing.border.LineBorder;
@@ -18,31 +20,28 @@ public class EquipmentView extends ItemDisplayView {
 
             this.setBackground(new Color(0f, 0f, 0f, 0f));
             this.setBorder(new LineBorder(Color.WHITE));
+
         }
 
         public void paintComponent(Graphics g) {
 
             eds = new EquipmentDisplayStructure(this.getWidth(), this.getHeight());
+
             int width = this.getWidth();
             int height = this.getHeight();
-            int horLineLength = width/4;
-            int vertLineLength = height/4;
-            int boxWidth = width/10;
-            int boxHeight = height/10;
-            int x = horLineLength-boxWidth/2;
-            int y = vertLineLength-boxHeight/2;
             g.setColor(Color.WHITE);
-
             g.drawLine(width/2, 0, width/2, height);
             g.drawLine(0, height/2, width, height/2);
-            g.drawRect(2*horLineLength-boxWidth/2, y, boxWidth, boxHeight);
-            //getIvoList().get(0).paintComponent(g, 2*x, y, boxWidth, boxHeight);
-            for(int i=1; i<=3; i++) {
-                g.drawRect(i*horLineLength-boxWidth/2, 2*vertLineLength-boxHeight/2, boxWidth, boxHeight);
-                //getIvoList().get(i).paintComponent(g, i*x, 2*y, boxWidth, boxHeight);
+            g.drawRect(eds.getHelmetX(), eds.getHelmetY(), eds.getSlotWidth(), eds.getslotHeight());
+            g.drawRect(eds.getWeaponX(), eds.getWeaponY(), eds.getSlotWidth(), eds.getslotHeight());
+            g.drawRect(eds.getChestX(), eds.getChestY(), eds.getSlotWidth(), eds.getslotHeight());
+            g.drawRect(eds.getShieldX(), eds.getShieldY(), eds.getSlotWidth(), eds.getslotHeight());
+            g.drawRect(eds.getLegsX(), eds.getLegsY(), eds.getSlotWidth(), eds.getslotHeight());
+
+            // TODO: 4/9/2016 Create a Slot class, where each Slot has an x and a y and then we call slot.paintComponent and the check for whether or not something is null happens in the slot
+            for (ItemViewObject evo: getIvoList()) {
+
             }
-            g.drawRect(2*horLineLength-boxWidth/2, 3*vertLineLength-boxHeight/2, boxWidth, boxHeight);
-            //getIvoList().get(i).paintComponent(g, 2*x, 3*y, boxWidth, boxHeight);
 
         }
 
