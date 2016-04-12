5c62749486d7abc089ac9d2c492d714aaa2cd86d
Qianwen Ding <lulu.dingq@gmail.com>
2016-04-12 17:39:36 -0400

    updated time model, use fuseable for time event that happens one time only

--- src/com/wrathOfLoD/GameClock/TimeModel.java
+++ src/com/wrathOfLoD/GameClock/TimeModel.java
@@ -1,15 +1,17 @@
 package com.wrathOfLoD.GameClock;
 
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Created by luluding on 4/9/16.
  */
 public class TimeModel {
+    //TODO: have alertable to remove Model side code duplication
+
     private List<Tickable> tickableList;
+    private ConcurrentHashMap<Fuseable, Integer> fuseableList;
 
     private static TimeModel instance = new TimeModel();
     public static TimeModel getInstance() {
@@ -18,6 +20,7 @@ public class TimeModel {
 
     private TimeModel(){
         tickableList = Collections.synchronizedList(new CopyOnWriteArrayList<Tickable>());
+        fuseableList = new ConcurrentHashMap<>();
     }
 
     public void tick(){
@@ -25,6 +28,16 @@ public class TimeModel {
         while(tickableIterator.hasNext()){
             tickableIterator.next().tick();
         }
+
+        Iterator<Map.Entry<Fuseable, Integer>> fuseableIterator = this.fuseableList.entrySet().iterator();
+        while(fuseableIterator.hasNext()){
+            Map.Entry<Fuseable, Integer> entry = fuseableIterator.next();
+            if(entry.getValue() <= 0){
+                deregisterFuseable(entry.getKey());
+                entry.getKey().explode();
+            }
+        }
+
     }
 
     public void registerTickable(Tickable tickable){
@@ -33,6 +46,14 @@ public class TimeModel {
 
     public void deregisterTickable(Tickable tickable){
         this.tickableList.remove(tickable);
+    }
+
+    public void registerFuseable(Fuseable fuseable, int ticks){
+        this.fuseableList.put(fuseable, ticks);
+    }
+
+    private void deregisterFuseable(Fuseable fuseable){
+        this.fuseableList.remove(fuseable);
     }
 
 
---
+++ src/com/wrathOfLoD/Models/RangedEffect/HitBox/HitBox.java
@@ -0,0 +1,7 @@
+package com.wrathOfLoD.Models.RangedEffect.HitBox;
+
+/**
+ * Created by luluding on 4/9/16.
+ */
+public class HitBox {
+}
