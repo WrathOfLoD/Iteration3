737bb121acca91adb22b01a26fb10de857c3a1e5
Qianwen Ding <lulu.dingq@gmail.com>
2016-04-13 18:57:16 -0400

    restructured Ability system

--- src/com/wrathOfLoD/Models/RangedEffect/REG/ConicalREG.java
+++ src/com/wrathOfLoD/Models/RangedEffect/REG/ConicalREG.java
@@ -1,7 +1,23 @@
 package com.wrathOfLoD.Models.RangedEffect.REG;
 
+import com.wrathOfLoD.Models.RangedEffect.HitBox.HitBoxFactory;
+import com.wrathOfLoD.Utility.Direction;
+import com.wrathOfLoD.Utility.Position;
+
+import java.util.List;
+
 /**
  * Created by luluding on 4/9/16.
  */
-public class ConicalREG {
+public class ConicalREG extends RangedEffectGenerator {
+
+    public ConicalREG(int totalDistance, Position entityLocation, int damage, int travelTime, HitBoxFactory hitBoxFactory){
+        super(totalDistance, entityLocation, damage, travelTime, hitBoxFactory);
+    }
+
+
+    @Override
+    public List<Position> getEffectiveLocations(int radius, Position orignalPos) {
+        return null;
+    }
 }
--- src/com/wrathOfLoD/Models/RangedEffect/REG/SphericalREG.java
+++ src/com/wrathOfLoD/Models/RangedEffect/REG/SphericalREG.java
@@ -1,7 +1,21 @@
 package com.wrathOfLoD.Models.RangedEffect.REG;
 
+import com.wrathOfLoD.Models.RangedEffect.HitBox.HitBoxFactory;
+import com.wrathOfLoD.Utility.Position;
+
+import java.util.List;
+
 /**
  * Created by luluding on 4/9/16.
  */
-public class SphericalREG {
+public class SphericalREG extends RangedEffectGenerator {
+
+    public SphericalREG(int totalDistance, Position entityLocation, int damage, int travelTime, HitBoxFactory hitBoxFactory){
+        super(totalDistance, entityLocation, damage, travelTime, hitBoxFactory);
+    }
+
+    @Override
+    public List<Position> getEffectiveLocations(int radius, Position orignalPos) {
+        return null;
+    }
 }
