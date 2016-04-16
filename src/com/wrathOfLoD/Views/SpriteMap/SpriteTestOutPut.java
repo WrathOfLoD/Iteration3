77d252cb50b218e7b56e213a33ffc168034a0248
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-04-16 18:21:18 -0400

    Add testing

---
+++ src/com/wrathOfLoD/Views/SpriteMap/SpriteTestOutPut.java
@@ -0,0 +1,13 @@
+package com.wrathOfLoD.Views.SpriteMap;
+
+import java.io.IOException;
+
+/**
+ * Created by matthewdiaz on 4/16/16.
+ */
+public class SpriteTestOutPut {
+    public static void main(String args[]) throws IOException {
+        SpriteMap sm = new SpriteMap();
+        sm.generateEntityMap();
+    }
+}
