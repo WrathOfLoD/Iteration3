1e812fb52713596449f8c1a6e74c55bcfe21671b
mburgunder <mitchellburgunder@yahoo.com>
2016-04-17 16:08:50 -0400

    Started Commands for Fog of War stuff Need to write the Position :: drawCircle() before I can complete these

---
+++ src/com/wrathOfLoD/Models/Commands/FogOfWarActionCommands/InvisibleTilesCommand.java
@@ -0,0 +1,14 @@
+package com.wrathOfLoD.Models.Commands.FogOfWarActionCommands;
+
+import com.wrathOfLoD.Models.Commands.ActionCommand;
+
+/**
+ * Created by Mitchell on 4/17/2016.
+ */
+public class InvisibleTilesCommand extends ActionCommand{
+	
+	@Override
+	public void execute(){
+
+	}
+}
