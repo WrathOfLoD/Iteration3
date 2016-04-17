4b4972e1c782969d44d1be36bd1c53dfcb4543a6
Qianwen Ding <lulu.dingq@gmail.com>
2016-04-17 18:17:06 -0400

    testing fan reg

--- src/com/wrathOfLoD/Models/RangedEffect/REG/FanREG.java
+++ src/com/wrathOfLoD/Models/RangedEffect/REG/FanREG.java
@@ -15,6 +15,13 @@ public class FanREG extends RangedEffectGenerator {
     public FanREG(int totalDistance, Position entityLocation, int damage, int travelTime, HitBoxFactory hitBoxFactory, Direction facingDirection){
         super(totalDistance, entityLocation, damage, travelTime, hitBoxFactory);
         this.facingDirection = facingDirection;
+
+        List<Position> pos = Position.drawArc(entityLocation, facingDirection, false, 3);
+        //List<Position> pos = Position.drawFan(entityLocation, facingDirection, true, 3,false);
+
+        for(Position p : pos){
+            System.out.println("HB POS: " + p.getQ() + " " + p.getR() + " " + p.getH());
+        }
     }
 
     @Override
