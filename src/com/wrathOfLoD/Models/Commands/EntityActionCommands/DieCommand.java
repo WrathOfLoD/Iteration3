36ed9b172395e1ad179a441119e68e7d6abe0d31
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-04-16 21:36:45 -0400

    DieCommand is implemented

--- src/com/wrathOfLoD/Models/Commands/EntityActionCommands/DieCommand.java
+++ src/com/wrathOfLoD/Models/Commands/EntityActionCommands/DieCommand.java
@@ -2,18 +2,20 @@ package com.wrathOfLoD.Models.Commands.EntityActionCommands;
 
 import com.wrathOfLoD.Models.Commands.ActionCommand;
 import com.wrathOfLoD.Models.Entity.Entity;
+import com.wrathOfLoD.Models.Map.Map;
 
 /**
  * Created by matthewdiaz on 4/8/16.
  */
 public class DieCommand  extends ActionCommand {
-    private Entity e;
+    private Entity entity;
 
-    public DieCommand(Entity e){
-        this.e = e;
+    public DieCommand(Entity entity){
+        this.entity = entity;
     }
     @Override
     public void execute() {
-
+        Map.getInstance().removeEntity(entity,entity.getPosition());
+        //todo: notify the view!!
     }
 }
