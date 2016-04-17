65a02550daceaafa301b0d6343e75252fe68164f
Icavitt <icavitt18@gmail.com>
2016-04-17 19:54:56 -0400

    beginning flying entity movement

--- src/com/wrathOfLoD/GameLaunching/LevelFactory.java
+++ src/com/wrathOfLoD/GameLaunching/LevelFactory.java
@@ -92,7 +92,7 @@ public class LevelFactory {
 //        Avatar.getInstance().equipAbility1(new FireballAbility(Avatar.getInstance(),5,10,3,5));
 
 //        ItemVendor.createHammer(mapAreaOne, new Position(1,2,9));
-        EntityVendor.createEnemy(new Position(1,2,9), mapAreaOne);
+//        EntityVendor.createEnemy(new Position(1,2,9), mapAreaOne);
 
 //        EntityVendor.createNewSmasherPlayer("Dave",new Position(0,0,8), mapAreaOne);
         EntityVendor.createNewSmasherPlayer("Dave",new Position(0,0,8), mapAreaOne);
