48721dd1bb209c2fda1b3d3cbca0693c9248db6e
mburgunder <mitchellburgunder@yahoo.com>
2016-04-18 07:09:48 -0400

    Working on Fog of War turning stuff gray doesn't work too well for the tiles

--- src/com/wrathOfLoD/Views/ViewObjects/TileViewObject.java
+++ src/com/wrathOfLoD/Views/ViewObjects/TileViewObject.java
@@ -7,7 +7,12 @@ import com.wrathOfLoD.Utility.Position;
 import com.wrathOfLoD.Views.SpriteMap.ImageAnimation;
 import javafx.geometry.Pos;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.FilteredImageSource;
+import java.awt.image.ImageFilter;
+import java.awt.image.ImageProducer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -28,12 +33,25 @@ public class TileViewObject extends ViewObject{
 		setImage(animation.getFrame()); //terrain
 	}
 
-	public void paintComponent(Graphics g, Position cameraCenter, Point screenCenter) {
+	public void paintComponent(Graphics g, Position cameraCenter, Point screenCenter, boolean visible) {
 		//super.paintComponent(g);
 		Point offset = Position.vectorSubtract(this.pos, cameraCenter).positionToXY();
 		this.setOffsetX(offset.x-40);
 		this.setOffsetY(offset.y-90);
-		g.drawImage(this.getImage(), this.getOffsetX() + screenCenter.x, this.getOffsetY() + screenCenter.y, 80, 140, null);
+
+		Image renderedImage = this.getImage();
+		if(!visible){
+			//ImageFilter filter = new GrayFilter(true, 50);
+			//ImageProducer producer = new FilteredImageSource(this.getImage().getSource(), filter);
+			//renderedImage = Toolkit.getDefaultToolkit().createImage(producer);
+			BufferedImage image = new BufferedImage(renderedImage.getWidth(null), renderedImage.getHeight(null), BufferedImage.TYPE_BYTE_GRAY);
+			Graphics g2 = image.getGraphics();
+			g2.setColor(new Color(0f,0f,0f,0.4f));
+			g2.drawImage(renderedImage, 0, 0, null);
+			//g2.dispose();
+			renderedImage = image;
+		}
+		g.drawImage(renderedImage, this.getOffsetX() + screenCenter.x, this.getOffsetY() + screenCenter.y, 80, 140, null);
 
 		Collections.sort(modelVOList, new Comparator<ModelViewObject>() {
 			@Override
@@ -44,7 +62,7 @@ public class TileViewObject extends ViewObject{
 
 		for (int i = 0; i < modelVOList.size(); i++){
 			ModelViewObject mvo = modelVOList.get(i);
-			mvo.paintComponent(g, this.getOffsetX() + screenCenter.x, this.getOffsetY() + screenCenter.y, mvo.getImage().getWidth(null), mvo.getImage().getHeight(null));
+			mvo.paintComponent(g, this.getOffsetX() + screenCenter.x, this.getOffsetY() + screenCenter.y, mvo.getImage().getWidth(null), mvo.getImage().getHeight(null), visible);
 		}
 
 //		for(ModelViewObject mvo : modelVOList){
